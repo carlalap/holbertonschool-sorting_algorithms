9426f09d18068ff68f8acec4cde792e02a5bbe83
carlalap <carlos3_alarcon@hotmail.com>
2023-04-21 11:51:57 -0700

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,51 @@
 #include "sort.h"
 
+
+/**
+ * swap - swaps two elements in an array
+ * @list: pointer to the head of the list
+ * @node1: pointer to the first node
+ * @node2: pointer to the second node
+ * Return: void
+ */
+void swap(listint_t **list, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*list = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
 /**
  * insertion_sort_list - Sort doubly linked list of integers in ascending
  *                       order using the Insertion sort algorithm.
  * @list: Pointer to a pointer to the head node of the list.
+ * Return: void
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	if (*list == NULL || (*list)->next == NULL)
+	listint_t *prev, *next;
+
+	if (list == NULL || *list == NULL)
 		return;
 
-	listint_t *current, *next_node, *prev_node, *temp_node;
-
-	for (current = (*list)->next; current != NULL; current = next_node)
+	for (next = (*list)->next; next != NULL; next = next->next)
 	{
-		next_node = current->next;
-
-		while (current->prev != NULL && current->n < current->prev->n)
+		prev = next->prev;
+		while (prev != NULL && prev->n > next->n)
 		{
-			prev_node = current->prev;
-			temp_node = current->next;
-
-			if (prev_node->prev != NULL)
-				prev_node->prev->next = current;
-			else
-				*list = current;
-
-			current->prev = prev_node->prev;
-			prev_node->prev = current;
-			prev_node->next = temp_node;
-
-			if (temp_node != NULL)
-				temp_node->prev = prev_node;
-
-			current->next = prev_node;
-			prev_node = current->prev;
-
+			swap(list, &prev, next);
 			print_list(*list);
 		}
 	}
 }
+
 
